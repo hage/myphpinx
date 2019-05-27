dd2c69ceac8cbffc505f46969f9b8927480cec4d
URA Takefumi <ura.takefumi@gmail.com>
2019-05-27 23:41:30 +0900

    [refactor] config.env の内容を $CONFIG グローバルに設定

--- app/html/index.php
+++ app/html/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../lib/config.php';
 echo '<pre>';
-print_r(config\get());
+print_r($CONFIG);
 echo '</pre>';
 phpinfo();
--- app/lib/config.php
+++ app/lib/config.php
@@ -1,20 +1,22 @@
 <?php
-namespace config;
 
-$V = null;
-
-function get() {
-  if (is_null($V)) {
-    $h = fopen('../config.env', 'r');
-    if ($h) {
-      $V = [];
-      while ($line = fgets($h)) {
-        if ($line) {
-          $e = explode('=', $line, 2);
-          $V[trim($e[0])] = trim($e[1]);
-        }
-      }
+function build_config_global_from_config_env()
+{
+  static $value = null;
+  if (!is_null($value)) {
+    return $value;
+  }
+  if (!($h = fopen('../config.env', 'r'))) {
+    return null;
+  }
+  $value = [];
+  while ($line = trim(fgets($h))) {
+    if ($line) {
+      $e = explode('=', $line, 2);
+      $value[trim($e[0])] = trim($e[1]);
     }
   }
-  return $V;
+  return $value;
 }
+
+$CONFIG = build_config_global_from_config_env();
